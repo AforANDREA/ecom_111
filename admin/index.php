c3356fac8e534a6559becdd7433483bdf3b3e25c
AforAndrea <lwinmoethant16@gmail.com>
2025-07-31 20:42:12 +0630

    PHP server working now and tested one file and installation finished.

---
+++ admin/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$batch = 111; // index datatype
+$name ="IMC"; // string datatype
+$temp = 33.5; // float datatype
+echo "<b><em>Hello Batch $batch, welcome to ecommerce module!</em></b>";
+
+?>
